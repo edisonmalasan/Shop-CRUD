7d1294da0e3d34da490300d8ae6be425045e7c53
edisonmalasan <malasanedison1@gmail.com>
2025-09-20 07:29:31 +0800

    added

---
+++ backend/src/main/java/com/edison/shop_crud/ShopCrudApplication.java
@@ -0,0 +1,13 @@
+package com.edison.shop_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCrudApplication.class, args);
+	}
+
+}
