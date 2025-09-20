c05d1bd74df56005700874ce708f5785549fea96
edisonmalasan <malasanedison1@gmail.com>
2025-09-20 08:36:01 +0800

    feat(frontend): axios

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,4 +2,8 @@ import axios from "axios";
 
 const API_URL = "https://api.placeholder.com";
 
-export const getProducts = () => axios.get(`${API_URL}/products`);
+export const getProducts = () => axios.get(API_URL);
+export const createProduct = (product) => axios.post(API_URL, product);
+export const updateProduct = (id, product) =>
+  axios.put(`${API_URL}/${id}`, product);
+export const deleteProduct = (id) => axios.delete(`${API_URL}/${id}`);
